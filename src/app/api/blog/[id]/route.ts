68bc061cf32251e043d0d70de91a299ad4e62b94
kawabata.blam <y.kawabata@blam.co.jp>
2023-09-08 21:04:01 +0900

    Create API for deleting blog

--- src/app/api/blog/[id]/route.ts
+++ src/app/api/blog/[id]/route.ts
@@ -33,3 +33,18 @@ export const PUT = async (req: Request, res: NextResponse) => {
     await prisma.$disconnect();
   }
 };
+
+// ブログ記事削除用のAPI
+export const DELETE = async (req: Request, res: NextResponse) => {
+  try {
+    const id: number = parseInt(req.url.split('/blog/')[1]);
+
+    await main();
+    const post = await prisma.post.delete({where : {id}})
+    return NextResponse.json({ message: 'Success!', post }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: 'Error', err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
