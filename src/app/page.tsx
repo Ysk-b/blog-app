de886b8611751e32ab3a220a9f231b3f6690884a
kawabata.blam <y.kawabata@blam.co.jp>
2023-09-08 21:31:38 +0900

    APIフェッチ関数の定義

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 import Link from 'next/link';
 import React from 'react';
+import { PostType } from './types/types';
 
-const Home = () => {
+const fetchAllBlogs = async () => {
+  const res = await fetch('http://localhost:3000/api/blogs', {
+    cache: 'no-store',
+  });
+
+  const data = await res.json();
+  return data.posts;
+};
+
+const Home = async () => {
+  const posts = await fetchAllBlogs();
+
   return (
     <main className='w-full h-full'>
       <div className='md:w-2/4 sm:w-3/4 m-auto p-4 my-5 rounded-lg bg-blue-900 drop-shadow-xl'>
@@ -17,27 +29,35 @@ const Home = () => {
       </div>
 
       <div className='w-full flex flex-col justify-center items-center'>
-        <div className='w-3/4 p-4 rounded-md mx-3 my-2 bg-slate-300 flex flex-col justify-center'>
-          <div className='flex items-center my-3'>
-            <div className='mr-auto'>
-              <h2 className='mr-auto font-semibold'>First Post</h2>
+        {posts.map((post: PostType) => {
+          <div
+            key={post.id}
+            className='w-3/4 p-4 rounded-md mx-3 my-2 bg-slate-300 flex flex-col justify-center'
+          >
+            <div className='flex items-center my-3'>
+              <div className='mr-auto'>
+                <h2 className='mr-auto font-semibold'>{post.title}</h2>
+              </div>
+              <Link
+                href={`/blog/edit/${post.id}`}
+                className='px-4 py-1 text-center text-xl bg-slate-900 rounded-md font-semibold text-slate-200'
+              >
+                編集
+              </Link>
             </div>
-            <Link
-              href={`/blog/edit/1`}
-              className='px-4 py-1 text-center text-xl bg-slate-900 rounded-md font-semibold text-slate-200'
-            >
-              編集
-            </Link>
-          </div>
 
-          <div className='mr-auto my-1'>
-            <blockquote className='font-bold text-slate-700'>2023/8/17</blockquote>
-          </div>
+            <div className='mr-auto my-1'>
+              <blockquote className='font-bold text-slate-700'>
+                {new Date(post.date).toDateString()}
+              </blockquote>
+            </div>
 
-          <div className='mr-auto my-1'>
-            <h2>First Post Description</h2>
-          </div>
-        </div>
+            <div className='mr-auto my-1'>
+              <h2>{post.description}</h2>
+            </div>
+          </div>;
+        })}
+        ;
       </div>
     </main>
   );
