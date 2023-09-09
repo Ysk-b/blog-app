00f9673893dfeca329ea56b3fc02a2639b06ae17
kawabata.blam <y.kawabata@blam.co.jp>
2023-09-09 17:03:00 +0900

    editBlogページ実装

--- src/app/blog/edit/[id]/page.tsx
+++ src/app/blog/edit/[id]/page.tsx
@@ -2,24 +2,21 @@
 
 import { useRouter } from 'next/navigation';
 import React, { useRef } from 'react';
-import { Toaster, toast } from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
 
-const postBlog = async (title: string | undefined, description: string | undefined) => {
-  const res = await fetch('http://localhost:3000/api/blog', {
-    method: 'POST',
+const editBlog = async (title: string | undefined, description: string | undefined, id: number) => {
+  const res = await fetch(`http://localhost:3000/api/blog/${id}`, {
+    method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ title, description }),
+    body: JSON.stringify({ title, description, id }),
   });
 
   return res.json();
 };
 
-// FIX ME
-// ロード中か否かを状態変数で保持
-// trueの場合toastを非表示にする
-const PostBlog = () => {
+const EditPost = ({ params }: { params: { id: number } }) => {
   const router = useRouter();
   const titleRef = useRef<HTMLInputElement | null>(null);
   const descriptionRef = useRef<HTMLTextAreaElement | null>(null);
@@ -27,10 +24,10 @@ const PostBlog = () => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    toast.loading('Now Posting...', { id: 'now-posting' });
-    await postBlog(titleRef.current?.value, descriptionRef.current?.value);
+    toast.loading('Now Editting...', { id: 'now-posting' });
+    await editBlog(titleRef.current?.value, descriptionRef.current?.value, params.id);
 
-    toast.success('Successfully Posted!', { id: 'successfully-posted' });
+    toast.success('Successfully Editted!', { id: 'successfully-posted' });
 
     router.push('/');
     router.refresh();
@@ -38,10 +35,10 @@ const PostBlog = () => {
 
   return (
     <>
-      <Toaster />
+      < Toaster/>
       <div className='w-full m-auto flex my-4'>
         <div className='flex flex-col justify-center items-center m-auto'>
-          <p className='text-2xl text-slate-200 font-bold p-3'>ブログ新規作成 🚀</p>
+          <p className='text-2xl text-slate-200 font-bold p-3'>ブログの編集 🚀</p>
           <form onSubmit={handleSubmit}>
             <input
               ref={titleRef}
@@ -55,7 +52,10 @@ const PostBlog = () => {
               className='rounded-md px-4 py-2 w-full my-2'
             ></textarea>
             <button className='font-semibold px-4 py-2 shadow-xl bg-slate-200 rounded-lg m-auto hover:bg-slate-100'>
-              投稿
+              更新
+            </button>
+            <button className='ml-2 font-semibold px-4 py-2 shadow-xl bg-red-400 rounded-lg m-auto hover:bg-slate-100'>
+              削除
             </button>
           </form>
         </div>
@@ -64,4 +64,4 @@ const PostBlog = () => {
   );
 };
 
-export default PostBlog;
+export default EditPost;
