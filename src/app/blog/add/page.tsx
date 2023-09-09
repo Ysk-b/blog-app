1839658845527bfaffa0f441228576c45da56d83
kawabata.blam <y.kawabata@blam.co.jp>
2023-09-09 15:31:40 +0900

    PostBlogページの実装

---
+++ src/app/blog/add/page.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import React, { useRef } from 'react';
+
+const PostBlog = () => {
+  const titleRef = useRef<HTMLInputElement | null>(null);
+  const descriptionRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    
+    console.log(titleRef);
+    console.log(descriptionRef);
+  };
+
+  return (
+    <div className='w-full m-auto flex my-4'>
+      <div className='flex flex-col justify-center items-center m-auto'>
+        <p className='text-2xl text-slate-200 font-bold p-3'>ブログ新規作成 🚀</p>
+        <form onSubmit={handleSubmit}>
+          <input
+            ref={titleRef}
+            placeholder='タイトルを入力'
+            type='text'
+            className='rounded-md px-4 w-full py-2 my-2'
+          />
+          <textarea
+            ref={descriptionRef}
+            placeholder='記事詳細を入力'
+            className='rounded-md px-4 py-2 w-full my-2'
+          ></textarea>
+          <button className='font-semibold px-4 py-2 shadow-xl bg-slate-200 rounded-lg m-auto hover:bg-slate-100'>
+            投稿
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default PostBlog;
